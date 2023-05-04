b1ba9df2a8596981af1b59bef002910b1c7798cb
Rachida Oussakel <rachida.oussakel@gmail.com>
2023-05-04 20:48:33 +0200

    Filter

--- frontend/src/Components/Filter.js
+++ frontend/src/Components/Filter.js
@@ -1,80 +1,86 @@
 import React from 'react'
 import './Filter.css';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 
-const FilterOptions = ({keywords,setKeywords}) => {
+const FilterOptions = ({ keywords, setKeywords }) => {
   const Tag = [
-      "Animals",
-      "People",
-      "Nature", 
-      "Buildings", 
-      "Food",
-    ];
-    const options = [
-      {
-        title: "Tag",
-        options: [
-          "Animals",
-          "People",
-          "Nature", 
-          "Buildings", 
-          "Food",
-        ],
-        height : 150
-      },
-      {
-        title: "Location",
-        options: ["Indoor", "Outdoor"],
-        height : 40
-      },
-     
-    ];
-    const handleChange = (e)=>{
-      if (e.target.checked) {
-        setKeywords(prev=>([...prev,e.target.name]));
-      }
-      else {
-        setKeywords(prev=>prev.filter(p=>e.target.name!==p));
-      }
+    "Animal",
+    "People",
+    "Nature",
+    "Buildings",
+    "Food",
+  ];
+  const options = [
+    {
+      title: "Tag",
+      options: [
+        "Animal",
+        "People",
+        "Nature",
+        "Buildings",
+        "Food",
+      ],
+      height: 190
+    },
+    {
+      title: "Location",
+      options: ["Indoor", "Outdoor"],
+      height: 40
+    },
+
+  ];
+  const handleChange = (e) => {
+    if (e.target.checked) {
+      setKeywords(prev => ([...prev, e.target.name]));
     }
-    
-return (
-  <div className="filter">
-  <div className="items">
-    <div className="text1">Filters</div>
-    <div className="text2">Clear All</div>
-  </div>
-  {options.map((option,idx) => (
-    <div key={idx}>
-      <hr className="mt-5" />
-      <div>
-        <div className="option">{option.title}</div>
-        <div className="mt-2">
-          <input
-            type="text"
-            className="border"
-            placeholder={`Search by ${option.title}`}
-          />
-        </div>
-        <div className="mt-4" style={{height :` ${option.height}px`}}>
-          {option.options.map((name,idx) => (
-            <div key={idx} className="space-x-2">
-              <div className="inputOption">
-                <input
-                  type="checkbox"
-                  name={name}
-                  onChange={handleChange}
-                  className="checkbox"
-                />
-              </div>
-              <div className="names">{name}</div>
-            </div>
-          ))}
-        </div>
+    else {
+      setKeywords(prev => prev.filter(p => e.target.name !== p));
+    }
+    console.log(keywords)
+  }
+
+  return (
+    <div className="filter">
+      <div className="items">
+        <div className="text1">Filters</div>
       </div>
+      {options.map((option, idx) => (
+        <div key={idx}>
+          <hr className="mt-5" />
+          <div>
+            <div className="option">{option.title}</div>
+
+            <div className="mt-4" style={{ height: ` ${option.height}px` }}>
+              {option.options.map((name, idx) => (
+                <div key={idx} className="space-x-2">
+                  <div className="inputOption">
+                    <FormControlLabel 
+                    sx = {{
+                      marginBottom : '-30px',
+                    }} 
+                    required 
+                    control={
+                    <Checkbox 
+                    sx = {{
+                      '&.Mui-checked': {
+                        color: "#78909C",
+                      },
+                    }} 
+                    name = {name} 
+                    onChange={handleChange} />
+                  } 
+                    label={name} 
+                    />
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
-  ))}
-</div>
-)
+  )
 }
 
 export default FilterOptions
--- frontend/src/Components/feed.js
+++ frontend/src/Components/feed.js
@@ -2,13 +2,41 @@ import React from "react";
 import './Feed.css';
 import { useState } from 'react';
 import FilterOptions from "./Filter";
+import PrimarySearchAppBar from "./ProfileBar";
+import HomeIcon from '@mui/icons-material/Home';
 
 
 function Feed(props) {
- 
+
 
   const [imagePreview, setImagePreview] = useState("");
   const [keywords, setKeywords] = useState([]);
+  var cards = [
+    {
+      name: "Lena Rose",
+      profile_picture: "profile.jpg",
+      post: "Fox.jpg",
+      likes: "50 likes",
+      tag: "Fox",
+      tag_general : "Animal"
+    }, {
+      name: "John Doe",
+      profile_picture: "profile2.jpg",
+      post: "plant.webp",
+      likes: "30 likes",
+      tag: "Plant",
+      tag_general: "Nature"
+    }, {
+      name: "Alex Sabatier",
+      profile_picture: "profile2.jpg",
+      post: "avocat.jpg",
+      likes: "3 likes",
+      tag: "Avocat",
+      tag_general: "Food"
+    }
+
+
+  ]
 
   function previewImage(event) {
     const file = event.target.files[0];
@@ -20,230 +48,102 @@ function Feed(props) {
 
     reader.readAsDataURL(file);
   }
-
-  function handleclick(){
-   /*to display the popup*/
-   const cameraBtn = document.querySelector('#camera-btn');
-   const popup = document.querySelector('.popup');
-   if(cameraBtn!=null && popup!=null){
-     cameraBtn.addEventListener('click', () => {
-     popup.style.display = 'flex';
-   });}
   
-   /**to remove the popup */
-   const camBtn = document.querySelector('#btn-close');
-   const pop = document.querySelector('.popup');
-   if(camBtn!=null && pop!=null){
-   camBtn.addEventListener('click', () => {
-     pop.style.display = 'none'; 
-   });}
-  }
-    return (
-    <> 
-  <nav>
-      <ul>
-       <li>
-        <a onClick={() => props.setShowFeed(true)}>
-        <img className="icon" src="maison.png"/>
-        <h6 className="text">Home</h6>
-        </a>
-       </li>
+  
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <a onClick={() => props.setShowFeed(true)}>
+              <img className="icon" src="maison.png" />
+              <h6 className="text">Home</h6>
+            </a>
+          </li>
 
-       <li>
-        <a onClick={() => props.setShowProfile(true)}>
-        <img className="icon" src="utilisateur.png"/>
-        <h6 className="text">Profile</h6>
-        </a>
-       </li>
-       <li>
-       <a onClick={() => props.setShowSavedPosts(true)}>
-        <img className="icon" src="bookmark.png"/>
-        <h6 className="text">Saved</h6>
-        </a>
-       </li>
-       <li>
-        <a href="#">
-        <img className="icon" src="gear.png"/>
-        <h6 className="text" >Settings</h6>
-        </a>
-       </li>
-       <li>
-        <a href="#">
-        <img className="icon" src="aidez-moi.png"/>
-        <h6 className="text">Help</h6>
-        </a>
-       </li>
-       <li>
-        <a href="./Login" className="logout">
-        <img className="icon" src="logout.png"/>
-        <h6 className="text">Log out</h6>
-        </a>
-       </li>
-      </ul>
-    </nav>
-    
-    <div className="search-bar">
-          <button id="search-button"><img id="search-img" src="magnifier.png"/></button>
-          <input type="text" placeholder="Search by user's name"></input>
+          <li>
+            <a onClick={() => props.setShowProfile(true)}>
+              <img className="icon" src="utilisateur.png" />
+              <h6 className="text">Profile</h6>
+            </a>
+          </li>
+          <li>
+            <a onClick={() => props.setShowSavedPosts(true)}>
+              <img className="icon" src="bookmark.png" />
+              <h6 className="text">Saved</h6>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img className="icon" src="gear.png" />
+              <h6 className="text" >Settings</h6>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img className="icon" src="aidez-moi.png" />
+              <h6 className="text">Help</h6>
+            </a>
+          </li>
+          <li>
+            <a href="./Login" className="logout">
+              <img className="icon" src="logout.png" />
+              <h6 className="text">Log out</h6>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <PrimarySearchAppBar></PrimarySearchAppBar>
+      
+      <div className="popup">
+        <div className="wrapper">
+          <button id="btn-close"><img id="img-close" src="cross.png" /></button>
+          <header>Drag & Drop a picture </header>
+          <button id="dragdrop-btn"><img id="dragdrop-icon" src="cloud-upload.png" /></button>
+          <form id="form1" action="/uploadfile" enctype="multipart/form-data" method="post">
+            <input type="file" id="file-input" className="file-input" accept="image/*" onchange={previewImage} />
+            <button id="submit1" type="button" >Share & Classify</button>
+          </form>
+          <div id="image-preview"><img src={imagePreview} /></div>
+        </div>
       </div>
-    <button id="camera-btn" ><img id="camera" src="camera.png" onClick={handleclick}/></button> 
+      <FilterOptions keywords={keywords} setKeywords={setKeywords} />
 
-  <div className="popup">
-  <div className="wrapper">
-  <button id="btn-close"><img id="img-close" src="cross.png"/></button>
-    <header>Drag & Drop a picture </header> 
-    <button id="dragdrop-btn"><img id="dragdrop-icon" src="cloud-upload.png"/></button>
-    <form id="form1" action="/uploadfile" enctype="multipart/form-data" method="post">
-      <input type="file" id="file-input" className="file-input" accept="image/*" onchange={previewImage}/>
-      <button id="submit1" type="button" >Share & Classify</button>
-    </form>
-    <div id="image-preview"><img src={imagePreview} /></div>
-  </div>
-</div>
-<FilterOptions keywords={keywords} setKeywords={setKeywords} />
- <div className="cards">
-      <div class="card">
-        <div className="profile">
-            <img className="profile-pic" src="profile.jpg"/>
-            <h4 className="Name"><b>Lena Rose</b></h4> 
-        </div>
-        <img className="post" src="Fox.jpg" alt="Avatar" />
-        <p className="type">FOX</p>
-        <div className="barre">
-          <div>
-          <button> <img  src="like.png" /></button>
-          <h6>50 likes</h6>
-          </div>
-          <div >
-          <button><img src="categorization.png" /></button>
-          <h6>Animal: Fox</h6>
-          </div>
-          <div>
-          <button><img src="bookmark.png" /></button>
-          <h6>save</h6>
-          </div>
-        </div>
-      </div>
-    
-      <div class="card">
-      <div className="profile">
-          <img className="profile-pic" src="profile2.jpg"/>
-          <h4 className="Name"><b>John Doe</b></h4> 
-      </div> 
-      <img className="post" src="plant.webp" alt="Avatar" />
-      <p className="type">PLANT</p>
-      <div className="barre">
-        <div>
-        <button><img  src="like.png" /></button>
-        <h6>50 likes</h6>
-        </div>
-        <div >
-        <button><img src="categorization.png" /></button>
-        <h6>Plant </h6>
-        </div>
-        <div>
-        <button><img src="bookmark.png" /></button>
-        <h6>save</h6>
-        </div>
-      </div>
+      <div className="cards">
+        {
+          cards.filter(card => keywords.length === 0 || keywords.includes(card.tag_general) ).map(card => {
+            return (
+              <div class="card">
+                <div className="profile">
+                  <img className="profile-pic" src={card.profile_picture} />
+                  <h4 className="Name"><b>{card.name}</b></h4>
+                </div>
+                <img className="post" src={card.post} alt="Avatar" />
+                <p className="type">{card.tag}</p>
+                <div className="barre">
+                  <div>
+                    <button> <img src="like.png" /></button>
+                    <h6>{card.likes}</h6>
+                  </div>
+                  <div >
+                    <button><img src="categorization.png" /></button>
+                    <h6>{card.tag}</h6>
+                  </div>
+                  <div>
+                    <button><img src="bookmark.png" /></button>
+                    <h6>save</h6>
+                  </div>
+                </div>
+              </div>
+            );
+          })
+        }
+
       </div>
 
-      <div class="card">
-      <div className="profile">
-          <img className="profile-pic" src="profile.jpg"/>
-          <h4 className="Name"><b>Lena Rose</b></h4> 
-      </div> 
-      <img className="post" src="avocat.jpg" alt="Avatar" />
-      <p className="type">AVOCAT</p>
-      <div className="barre">
-        <div>
-        <button><img  src="like.png" /></button>
-        <h6>50 likes</h6>
-        </div>
-        <div >
-        <button><img src="categorization.png" /></button>
-        <h6>Fruit: Avocat</h6>
-        </div>
-        <div>
-        <button><img src="bookmark.png" /></button>
-        <h6>save</h6>
-        </div>
-      </div>
-      </div>
 
-      <div class="card">
-        <div className="profile">
-            <img className="profile-pic" src="profile.jpg"/>
-            <h4 className="Name"><b>Lena Rose</b></h4> 
-        </div>
-        <img className="post" src="Fox.jpg" alt="Avatar" />
-        <p className="type">FOX</p>
-        <div className="barre">
-          <div>
-          <button> <img  src="like.png" /></button>
-          <h6>50 likes</h6>
-          </div>
-          <div >
-          <button><img src="categorization.png" /></button>
-          <h6>Animal: Fox</h6>
-          </div>
-          <div>
-          <button><img src="bookmark.png" /></button>
-          <h6>save</h6>
-          </div>
-        </div>
-      </div>
-    
-      <div class="card">
-      <div className="profile">
-          <img className="profile-pic" src="profile2.jpg"/>
-          <h4 className="Name"><b>John Doe</b></h4> 
-      </div> 
-      <img className="post" src="plant.webp" alt="Avatar" />
-      <p className="type">PLANT</p>
-      <div className="barre">
-        <div>
-        <button><img  src="like.png" /></button>
-        <h6>50 likes</h6>
-        </div>
-        <div >
-        <button><img src="categorization.png" /></button>
-        <h6>Plant</h6>
-        </div>
-        <div>
-        <button><img src="bookmark.png" /></button>
-        <h6>save</h6>
-        </div>
-      </div>
-      </div>
-
-      <div class="card">
-      <div className="profile">
-          <img className="profile-pic" src="profile.jpg"/>
-          <h4 className="Name"><b>Lena Rose</b></h4> 
-      </div> 
-      <img className="post" src="avocat.jpg" alt="Avatar" />
-      <p className="type">AVOCAT</p>
-      <div className="barre">
-        <div>
-        <button><img  src="like.png" /></button>
-        <h6>50 likes</h6>
-        </div>
-        <div >
-        <button><img src="categorization.png" /></button>
-        <h6>Fruit: Avocat</h6>
-        </div>
-        <div>
-        <button><img src="bookmark.png" /></button>
-        <h6>save</h6>
-        </div>
-      </div>
-      </div>
-
-  </div>
-
-  </>
-    )
+    </>
+  )
 }
 
 export default Feed
